237c2ebb242746e17cdbe0fe447d79c006705abe
Karthik <bkarthiklal@gmail.com>
2021-04-06 01:17:15 +0530

    Updating Default layout and adding a background

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,91 +1,151 @@
 <template>
-  <v-app dark>
-    <v-navigation-drawer
-      v-model="drawer"
-      :mini-variant="miniVariant"
-      :clipped="clipped"
-      fixed
-      app
-    >
-      <v-list>
-        <v-list-item
-          v-for="(item, i) in items"
-          :key="i"
-          :to="item.to"
-          router
-          exact
-        >
-          <v-list-item-action>
-            <v-icon>{{ item.icon }}</v-icon>
-          </v-list-item-action>
-          <v-list-item-content>
-            <v-list-item-title v-text="item.title" />
-          </v-list-item-content>
-        </v-list-item>
-      </v-list>
-    </v-navigation-drawer>
-    <v-app-bar :clipped-left="clipped" fixed app>
-      <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
-      <v-btn icon @click.stop="miniVariant = !miniVariant">
-        <v-icon>mdi-{{ `chevron-${miniVariant ? 'right' : 'left'}` }}</v-icon>
-      </v-btn>
-      <v-btn icon @click.stop="clipped = !clipped">
-        <v-icon>mdi-application</v-icon>
-      </v-btn>
-      <v-btn icon @click.stop="fixed = !fixed">
-        <v-icon>mdi-minus</v-icon>
-      </v-btn>
-      <v-toolbar-title v-text="title" />
-      <v-spacer />
-      <v-btn icon @click.stop="rightDrawer = !rightDrawer">
-        <v-icon>mdi-menu</v-icon>
-      </v-btn>
-    </v-app-bar>
-    <v-main>
-      <v-container>
+  <v-app>
+    <div
+      id="background"
+      class="background-img"
+      :style="{
+        backgroundColor: this.color,
+        transform: this.skew,
+        width: this.width,
+        marginLeft: this.margin,
+      }"
+    ></div>
+    <Navbar :name="Name" />
+
+    <v-main class="background-img">
+      <transition name="page-animate">
+        <!-- <router-view
+          @updateUser="updateUser"
+          @changePage="updatePage($event)"
+        ></router-view> -->
         <nuxt />
-      </v-container>
+      </transition>
     </v-main>
-    <v-navigation-drawer v-model="rightDrawer" :right="right" temporary fixed>
-      <v-list>
-        <v-list-item @click.native="right = !right">
-          <v-list-item-action>
-            <v-icon light> mdi-repeat </v-icon>
-          </v-list-item-action>
-          <v-list-item-title>Switch drawer (click me)</v-list-item-title>
-        </v-list-item>
-      </v-list>
-    </v-navigation-drawer>
-    <v-footer :absolute="!fixed" app>
-      <span>&copy; {{ new Date().getFullYear() }}</span>
-    </v-footer>
   </v-app>
 </template>
 
 <script>
+import Navbar from '@/components/Navbar.vue'
+
 export default {
+  name: 'App',
+
+  components: {
+    Navbar,
+  },
   data() {
     return {
-      clipped: false,
-      drawer: false,
-      fixed: false,
-      items: [
-        {
-          icon: 'mdi-apps',
-          title: 'Welcome',
-          to: '/',
-        },
-        {
-          icon: 'mdi-chart-bubble',
-          title: 'Inspire',
-          to: '/inspire',
-        },
-      ],
-      miniVariant: false,
-      right: true,
-      rightDrawer: false,
-      title: 'Vuetify.js',
+      page: 1,
+      color: '#fff',
+      skew: 'skewX(0)',
+      width: '',
+      margin: '',
+      Name: '',
     }
+  },
+
+  created() {},
+
+  methods: {
+    updatePage(page) {
+      if (window.innerWidth <= 1024) {
+        if (page === 1) {
+          this.color = 'rgba(255, 255, 255, 0.725)'
+          this.skew = 'skewX(30deg)'
+          this.width = '0%'
+          this.margin = '0'
+        }
+      } else if (page === 1) {
+        this.color = 'rgba(255, 255, 255, 0.725)'
+        this.skew = 'skewX(30deg)'
+        this.width = '70%'
+        this.margin = '-300px'
+      }
+
+      if (page === 2) {
+        this.color = 'rgba(255, 255, 255, 0.725)'
+        this.skew = 'skewX(0)'
+        this.width = '100%'
+        this.margin = '0'
+      }
+      if (page === 3) {
+        this.color = 'rgba(255, 255, 255, 0.725)'
+        this.skew = 'skewX(0)'
+        this.width = '100%'
+        this.margin = '0'
+      }
+    },
+    updateUser(name) {
+      this.Name = name.charAt(0).toUpperCase() + name.slice(1)
+    },
   },
 }
 </script>
+
+<style>
+#background {
+  width: 0;
+  height: 100vh;
+  position: absolute;
+  background-color: rgba(255, 255, 255, 0.7);
+  transform: skewX(0);
+  transition: 0.3s ease-in-out;
+}
+html {
+  overflow: hidden;
+}
+.background-img {
+  /* background: url('/bg.jpg'); */
+  background: url('https://images.unsplash.com/photo-1508615070457-7baeba4003ab?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1050&q=80');
+  background-size: cover;
+}
+
+.page-animate-enter-active {
+  animation: coming 0.2s;
+  animation-delay: 0.3s;
+  opacity: 0;
+}
+
+.page-animate-leave-active {
+  animation: going 0.3s;
+}
+.page {
+  text-align: center;
+  position: fixed;
+  width: 100%;
+}
+.snackbarToFront {
+  position: absolute;
+  z-index: 10;
+}
+
+@keyframes coming {
+  from {
+    opacity: 0;
+    transform: scale(1.1);
+  }
+
+  to {
+    transform: scale(1);
+    opacity: 1;
+  }
+}
+
+@keyframes going {
+  from {
+    opacity: 1;
+    transform: scale(1);
+  }
+
+  to {
+    transform: scale(0.9);
+    opacity: 0;
+  }
+}
+
+@media (max-height: 800px) {
+  html {
+    overflow-y: auto;
+  }
+}
+</style>
